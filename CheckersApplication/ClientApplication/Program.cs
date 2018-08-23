c33b9c081b5debdeb64d6922baf04952ed42a590
Unknown <wbrown026@gmail.com>
2018-08-23 04:40:56 -0400

    Testing

--- CheckersApplication/ClientApplication/Program.cs
+++ CheckersApplication/ClientApplication/Program.cs
@@ -17,14 +17,16 @@ namespace ClientApplication
             //string folder = Environment.CurrentDirectory;
             //System.Diagnostics.Process.Start(folder + @"\ServerApplication.exe");
 
-            Console.Title = "Client";
-            Client client = new Client();
-            client.ConnectToServer(); //2. START TRYING TO CONNECT TO SERVER
-            client.Exit();
+            Application.Run(new Main_Menu_Form());
+
+            //Console.Title = "Client";
+            //Client client = new Client();
+            //client.ConnectToServer(); //2. START TRYING TO CONNECT TO SERVER
+            
+            //client.Exit();
 
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
         }
 
     }
