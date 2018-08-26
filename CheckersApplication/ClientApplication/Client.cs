9d8333207d0e84a881a5e9c2f858b1b6063882c2
Vincent Finn <vincent.s.finn@gmail.com>
2018-08-26 16:41:45 -0400

    apperently one line didnt get pushed

--- CheckersApplication/ClientApplication/Client.cs
+++ CheckersApplication/ClientApplication/Client.cs
@@ -245,6 +245,7 @@ namespace ClientApplication
                             ck2 = new CKPoint(4, 3);
                             break;
                         case 4:
+                            ck1 = new CKPoint(3, 2);
                             ck2 = new CKPoint(5, 0);
                             break;
                         case 5:
