b4bf5b7c7b95a9f2aa822fb459eb972188d15edf
Unknown <wbrown026@gmail.com>
2018-08-27 20:07:03 -0400

    UI Fixes
    Changed the color of pieces and board and made the window more compact.

--- CheckersApplication/ClientApplication/Game_Form.Designer.cs
+++ CheckersApplication/ClientApplication/Game_Form.Designer.cs
@@ -23,7 +23,7 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.Exit = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game_Form));
             this.tbTurn = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.Board = new System.Windows.Forms.PictureBox();
@@ -31,25 +31,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.Board)).BeginInit();
             this.SuspendLayout();
             // 
-            // Exit
-            // 
-            this.Exit.AutoSize = true;
-            this.Exit.Font = new System.Drawing.Font("Corbel", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.Exit.Location = new System.Drawing.Point(917, -6);
-            this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(28, 36);
-            this.Exit.TabIndex = 3;
-            this.Exit.Text = "x";
-            this.Exit.Click += new System.EventHandler(this.Exit_Click);
-            // 
             // tbTurn
             // 
             this.tbTurn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(33)))));
             this.tbTurn.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbTurn.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbTurn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.tbTurn.Location = new System.Drawing.Point(34, 38);
+            this.tbTurn.Location = new System.Drawing.Point(12, 38);
             this.tbTurn.Name = "tbTurn";
             this.tbTurn.ReadOnly = true;
             this.tbTurn.Size = new System.Drawing.Size(217, 33);
@@ -63,7 +51,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.button1.Location = new System.Drawing.Point(71, 118);
+            this.button1.Location = new System.Drawing.Point(58, 118);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 50);
             this.button1.TabIndex = 7;
@@ -73,8 +61,8 @@
             // 
             // Board
             // 
-            this.Board.Image = global::ClientApplication.Properties.Resources.Checker_Board;
-            this.Board.Location = new System.Drawing.Point(318, 20);
+            this.Board.Image = ((System.Drawing.Image)(resources.GetObject("Board.Image")));
+            this.Board.Location = new System.Drawing.Point(234, 20);
             this.Board.Name = "Board";
             this.Board.Size = new System.Drawing.Size(579, 587);
             this.Board.TabIndex = 5;
@@ -86,7 +74,7 @@
             this.Reset_Move_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Reset_Move_Button.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Reset_Move_Button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.Reset_Move_Button.Location = new System.Drawing.Point(71, 174);
+            this.Reset_Move_Button.Location = new System.Drawing.Point(58, 174);
             this.Reset_Move_Button.Name = "Reset_Move_Button";
             this.Reset_Move_Button.Size = new System.Drawing.Size(127, 50);
             this.Reset_Move_Button.TabIndex = 8;
@@ -99,14 +87,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(33)))));
-            this.ClientSize = new System.Drawing.Size(944, 821);
+            this.ClientSize = new System.Drawing.Size(823, 611);
             this.Controls.Add(this.Reset_Move_Button);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tbTurn);
-            this.Controls.Add(this.Exit);
             this.Controls.Add(this.Board);
             this.Name = "Game_Form";
-            this.Text = "Game_Form";
+            this.Text = "King Me - v1.6";
             this.Shown += new System.EventHandler(this.Game_Form_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.Board)).EndInit();
             this.ResumeLayout(false);
@@ -115,8 +102,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label Exit;
         private System.Windows.Forms.TextBox tbTurn;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox Board;
--- CheckersApplication/ClientApplication/Main_Menu_Form.Designer.cs
+++ CheckersApplication/ClientApplication/Main_Menu_Form.Designer.cs
@@ -30,7 +30,6 @@
             this.Host_Button = new System.Windows.Forms.Button();
             this.Menu_Title = new System.Windows.Forms.Label();
             this.Menu_Icon = new System.Windows.Forms.PictureBox();
-            this.Exit = new System.Windows.Forms.Label();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.Menu_Panel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Menu_Icon)).BeginInit();
@@ -46,7 +45,7 @@
             this.Menu_Panel.Dock = System.Windows.Forms.DockStyle.Left;
             this.Menu_Panel.Location = new System.Drawing.Point(0, 0);
             this.Menu_Panel.Name = "Menu_Panel";
-            this.Menu_Panel.Size = new System.Drawing.Size(784, 821);
+            this.Menu_Panel.Size = new System.Drawing.Size(765, 611);
             this.Menu_Panel.TabIndex = 0;
             // 
             // Join_Button
@@ -55,7 +54,7 @@
             this.Join_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Join_Button.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Join_Button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.Join_Button.Location = new System.Drawing.Point(300, 427);
+            this.Join_Button.Location = new System.Drawing.Point(289, 387);
             this.Join_Button.Name = "Join_Button";
             this.Join_Button.Size = new System.Drawing.Size(180, 64);
             this.Join_Button.TabIndex = 3;
@@ -69,7 +68,7 @@
             this.Host_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Host_Button.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Host_Button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.Host_Button.Location = new System.Drawing.Point(300, 334);
+            this.Host_Button.Location = new System.Drawing.Point(289, 304);
             this.Host_Button.Name = "Host_Button";
             this.Host_Button.Size = new System.Drawing.Size(180, 64);
             this.Host_Button.TabIndex = 2;
@@ -82,49 +81,36 @@
             this.Menu_Title.AutoSize = true;
             this.Menu_Title.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Menu_Title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.Menu_Title.Location = new System.Drawing.Point(249, 189);
+            this.Menu_Title.Location = new System.Drawing.Point(260, 189);
             this.Menu_Title.Name = "Menu_Title";
-            this.Menu_Title.Size = new System.Drawing.Size(285, 80);
+            this.Menu_Title.Size = new System.Drawing.Size(241, 80);
             this.Menu_Title.TabIndex = 1;
-            this.Menu_Title.Text = "Checkers";
+            this.Menu_Title.Text = "King Me";
             // 
             // Menu_Icon
             // 
             this.Menu_Icon.Image = ((System.Drawing.Image)(resources.GetObject("Menu_Icon.Image")));
             this.Menu_Icon.Location = new System.Drawing.Point(0, 36);
             this.Menu_Icon.Name = "Menu_Icon";
-            this.Menu_Icon.Size = new System.Drawing.Size(784, 150);
+            this.Menu_Icon.Size = new System.Drawing.Size(765, 150);
             this.Menu_Icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.Menu_Icon.TabIndex = 0;
             this.Menu_Icon.TabStop = false;
-            // 
-            // Exit
-            // 
-            this.Exit.AutoSize = true;
-            this.Exit.Font = new System.Drawing.Font("Corbel", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(250)))));
-            this.Exit.Location = new System.Drawing.Point(917, -6);
-            this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(28, 36);
-            this.Exit.TabIndex = 2;
-            this.Exit.Text = "x";
-            this.Exit.Click += new System.EventHandler(this.Exit_Click);
             // 
             // Main_Menu_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(33)))));
-            this.ClientSize = new System.Drawing.Size(944, 821);
-            this.Controls.Add(this.Exit);
+            this.ClientSize = new System.Drawing.Size(823, 611);
             this.Controls.Add(this.Menu_Panel);
             this.Name = "Main_Menu_Form";
-            this.Text = "Main_Menu_Form";
+            this.Tag = "";
+            this.Text = "King Me - v1.6";
             this.Menu_Panel.ResumeLayout(false);
             this.Menu_Panel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Menu_Icon)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -133,7 +119,6 @@
         private System.Windows.Forms.Panel Menu_Panel;
         private System.Windows.Forms.PictureBox Menu_Icon;
         private System.Windows.Forms.Label Menu_Title;
-        private System.Windows.Forms.Label Exit;
         private System.Windows.Forms.Button Host_Button;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Button Join_Button;
